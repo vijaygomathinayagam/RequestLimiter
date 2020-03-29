748a49a2b36561466a277e162d54352df0092615
Vijay Gomathinayagam <vijay.gomathinayagam@hashouttech.com>
2020-03-29 08:08:34 +0530

    request ip address should be printed

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world!!")
+	http.HandleFunc("/", limitRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func limitRequest(w http.ResponseWriter, req *http.Request) {
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		log.Printf("Error spliting request remote address, %q", req.RemoteAddr)
+	}
+	userIP := net.ParseIP(ip)
+	fmt.Println(userIP)
 }
